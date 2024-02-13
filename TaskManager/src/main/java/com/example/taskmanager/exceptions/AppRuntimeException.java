0e0539880fafda324e3e1e852802d237c4635cef
Chi Lan Doan <chiland04@gmail.com>
2024-02-13 02:25:41 +0100

    Add default users and tasks

---
+++ TaskManager/src/main/java/com/example/taskmanager/exceptions/AppRuntimeException.java
@@ -0,0 +1,8 @@
+package com.example.taskmanager.exceptions;
+
+public class AppRuntimeException extends RuntimeException{
+
+    public AppRuntimeException(String message) {
+        super(message);
+    }
+}
