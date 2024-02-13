2b45e1cd080d9a13d637da833f55557fbda58d59
Chi Lan Doan <chiland04@gmail.com>
2024-02-13 02:31:50 +0100

    Create errorResponseDTO

---
+++ TaskManager/src/main/java/com/example/taskmanager/dtos/ErrorResponseDTO.java
@@ -0,0 +1,23 @@
+package com.example.taskmanager.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ErrorResponseDTO extends ResponseDTO{
+    @JsonProperty("error_message")
+    private String errorMessage;
+
+    public ErrorResponseDTO() {
+    }
+
+    public ErrorResponseDTO(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
